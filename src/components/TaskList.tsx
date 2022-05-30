8b44353ab7d620fbadb4a822aee7bbf5e750ddbd
Lucas França <lucasvpessoafranca@gmail.com>
2022-05-30 18:26:13 -0300

    finish

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -40,10 +40,10 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    const filteredTasks = tasks.filter(task =>task.id !== id);
+    const filterTasks = tasks.filter(task =>task.id !== id);
 
 
-    setTasks(filteredTasks)
+    setTasks(filterTasks)
   }
 
   return (
